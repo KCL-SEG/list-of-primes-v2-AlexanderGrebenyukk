fffc75828ab39793ce0858e08b83826baeb12c1e
Alexander Grebenyuk <alexander.grebenyuk@kcl.ac.uk>
2022-12-02 22:55:11 +0000

    done

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    currentNumber = 2
+    while(number_of_primes>len(list)):
+        prime=True
+
+        for i in range(2, currentNumber-1):
+            if(currentNumber%i==0):
+                prime = False
+                break
+
+        if prime:
+            listPrimes.append(currentNumber)
+            currentNumber = currentNumber + 1
+        else:
+            currentNumber = currentNumber + 1
     return list
