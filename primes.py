8e3131c29ea6a0cede6a59564ec91ca44ff8158e
Alexander Grebenyuk <alexander.grebenyuk@kcl.ac.uk>
2022-12-02 23:01:12 +0000

    done

--- primes.py
+++ primes.py
@@ -2,19 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    currentNumber = 2
-    while(number_of_primes>len(list)):
-        prime=True
+    if number_of_primes <= 0:
+        raise ValueError
+    else:
+        list = []
+        currentNumber = 2
+        while(number_of_primes>len(list)):
+            prime=True
 
-        for i in range(2, currentNumber-1):
-            if(currentNumber%i==0):
-                prime = False
-                break
+            for i in range(2, currentNumber-1):
+                if(currentNumber%i==0):
+                    prime = False
+                    break
 
-        if prime:
-            list.append(currentNumber)
-            currentNumber = currentNumber + 1
-        else:
-            currentNumber = currentNumber + 1
-    return list
+            if prime:
+                list.append(currentNumber)
+                currentNumber = currentNumber + 1
+            else:
+                currentNumber = currentNumber + 1
+        return list
